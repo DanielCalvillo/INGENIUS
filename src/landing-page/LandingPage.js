5ffa3009b14a9cf309c0f1e3875f09839b894b8c
DanielCalvillo-KMS-Tech <danielcalvillo@kms-technology.com>
2024-05-09 09:13:09 -0600

    fix

--- src/landing-page/LandingPage.js
+++ src/landing-page/LandingPage.js
@@ -19,47 +19,47 @@ import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
 import getLPTheme from "./getLPTheme";
 
-function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100dvw",
-        position: "fixed",
-        bottom: 24,
-      }}
-    >
-      <ToggleButtonGroup
-        color="primary"
-        exclusive
-        value={showCustomTheme}
-        onChange={toggleCustomTheme}
-        aria-label="Toggle design language"
-        sx={{
-          backgroundColor: "background.default",
-          "& .Mui-selected": {
-            pointerEvents: "none",
-          },
-        }}
-      >
-        <ToggleButton value>
-          <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
-          Custom theme
-        </ToggleButton>
-        <ToggleButton value={false}>Material Design 2</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
-  );
-}
+// function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+//   return (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         width: "100dvw",
+//         position: "fixed",
+//         bottom: 24,
+//       }}
+//     >
+//       <ToggleButtonGroup
+//         color="primary"
+//         exclusive
+//         value={showCustomTheme}
+//         onChange={toggleCustomTheme}
+//         aria-label="Toggle design language"
+//         sx={{
+//           backgroundColor: "background.default",
+//           "& .Mui-selected": {
+//             pointerEvents: "none",
+//           },
+//         }}
+//       >
+//         <ToggleButton value>
+//           <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
+//           Custom theme
+//         </ToggleButton>
+//         <ToggleButton value={false}>Material Design 2</ToggleButton>
+//       </ToggleButtonGroup>
+//     </Box>
+//   );
+// }
 
-ToggleCustomTheme.propTypes = {
-  showCustomTheme: PropTypes.shape({
-    valueOf: PropTypes.func.isRequired,
-  }).isRequired,
-  toggleCustomTheme: PropTypes.func.isRequired,
-};
+// ToggleCustomTheme.propTypes = {
+//   showCustomTheme: PropTypes.shape({
+//     valueOf: PropTypes.func.isRequired,
+//   }).isRequired,
+//   toggleCustomTheme: PropTypes.func.isRequired,
+// };
 
 export default function LandingPage() {
   const [mode, setMode] = React.useState("light");
@@ -71,9 +71,9 @@ export default function LandingPage() {
     setMode((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
-  const toggleCustomTheme = () => {
-    setShowCustomTheme((prev) => !prev);
-  };
+  // const toggleCustomTheme = () => {
+  //   setShowCustomTheme((prev) => !prev);
+  // };
 
   return (
     <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
